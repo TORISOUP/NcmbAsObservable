d407c2782013d72774be4d36a5bbb86cd6d31848
TORISOUP <tori.birdstrike@gmail.com>
2017-04-19 02:17:37 +0900

    Add LoginSample

---
+++ Assets/NcmbAsObservables/sample/LoginAndLogoutSample.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NcmbAsObservables;
+using UniRx;
+using UnityEngine;
+
+namespace Assets.NcmbAsObservables.sample
+{
+    public class LoginAndLogoutSample : MonoBehaviour
+    {
+        void Start()
+        {
+            ObservableFromNcmbUser
+                .LogInAsync("test_user_name", "hogehoge") //Login
+                .SelectMany(u => u.FetchAsyncAsStream())  //Fetch
+                .Do(user => Debug.Log(string.Format("{0}\t{1}", user.UserName, user.Email))) //Show result
+                .SelectMany(_ => ObservableFromNcmbUser.LogOutAsync()) //Log out
+                .Subscribe(_ => Debug.Log("Logged out"), e => Debug.LogError(e));
+        }
+    }
+}
